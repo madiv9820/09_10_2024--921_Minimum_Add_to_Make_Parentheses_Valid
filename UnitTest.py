c96ff4c2732c38a1910267e6f696575201d040f4
madiv9820 <themathur901@gmail.com>
2024-10-10 11:51:20 +0530

    Unit test cases for the problem

--- UnitTest.py
+++ UnitTest.py
@@ -10,13 +10,13 @@ class UnitTest(unittest.TestCase):
         return super().setUp()
     
     def test_Case1(self):
-        s, output = self.testCases[1]
+        s, output = self.testCases[1].values()
         result = self.obj.minAddToMakeValid(s)
         self.assertIsInstance(result, int)
         self.assertEqual(result, output)
 
     def test_Case2(self):
-        s, output = self.testCases[2]
+        s, output = self.testCases[2].values()
         result = self.obj.minAddToMakeValid(s)
         self.assertIsInstance(result, int)
         self.assertEqual(result, output)
