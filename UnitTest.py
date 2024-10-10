6dfc13ece143bcd09452edfa96e70117571d6ff4
madiv9820 <themathur901@gmail.com>
2024-10-10 11:47:33 +0530

    Unit Test cases for the Problem

---
+++ UnitTest.py
@@ -0,0 +1,24 @@
+from Solution import Solution
+import unittest
+from timeout_decorator import timeout
+
+class UnitTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testCases = {1: {'s': '())', 'output': 1},
+                          2: {'s': '(((', 'output': 3}}
+        self.obj = Solution()
+        return super().setUp()
+    
+    def test_Case1(self):
+        s, output = self.testCases[1]
+        result = self.obj.minAddToMakeValid(s)
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, output)
+
+    def test_Case2(self):
+        s, output = self.testCases[2]
+        result = self.obj.minAddToMakeValid(s)
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, output)
+
+if __name__ == '__main__': unittest.main()
